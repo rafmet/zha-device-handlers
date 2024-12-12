1d82e6f432fc60f8fe0b562bf1f4f2e94ca276e1
Rafal Metkowski <rafmet@ais.pl>
2024-12-12 20:53:30 +0100

    quirk for ts0603 garage door module

---
+++ zhaquirks/tuya/ts0603_garage.py
@@ -0,0 +1,16 @@
+from zhaquirks.tuya.builder import TuyaQuirkBuilder
+from zigpy.quirks.v2.homeassistant.binary_sensor import BinarySensorDeviceClass
+
+(
+    TuyaQuirkBuilder("_TZE608_fmemczv1", "TS0603")
+    .tuya_onoff(dp_id=1)
+    .tuya_binary_sensor(
+       dp_id=3,
+       device_class=BinarySensorDeviceClass.GARAGE_DOOR,
+       attribute_name="garage_door",
+       translation_key="garage_door",
+       fallback_name="Garage door",
+     )
+    .skip_configuration()
+    .add_to_registry()
+)
